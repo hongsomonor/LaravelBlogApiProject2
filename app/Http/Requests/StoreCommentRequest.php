1f15605578e1bb3a03b4952004554bda5893fd0b
hongsomonor <somonorhong011@gmail.com>
2025-11-01 21:48:54 +0700

    zxzxz

--- app/Http/Requests/StoreCommentRequest.php
+++ app/Http/Requests/StoreCommentRequest.php
@@ -8,7 +8,7 @@ class StoreCommentRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return true;
+        return true; // 
     }
 
     protected function prepareForValidation()
